9d153f1b6ad7dec62bc7ce232b2bff2e42679d1a
bvv023 <105537147+bvv023@users.noreply.github.com>
2024-04-01 12:44:57 +0000

    Deploying to gh-pages from @ bvv023/goit-js-hw-10@13eaaa91a5e0984e8bc87c51cff2540775629754 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{v as o}from"./assets/vendor-3cff2025.js";console.log("Is mango@mail.com a valid email?: ",o.isEmail("bvv023@mail.com"));console.log("Is bvv023dogmail.com a valid email?: ",o.isEmail("bvv023dogmail.com"));
+//# sourceMappingURL=commonHelpers3.js.map
